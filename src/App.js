a510a6a5d1c359d430c101490ccd42a74599dd92
Garrett Ross <garrettross@gGarrettRoss.attlocal.net>
2023-02-23 09:24:31 -0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Instructions from './components/Instructions'
+import Comments from './components/Comments'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Instructions />
+      <Comments />
     </div>
   );
 }
---
+++ src/components/Comments.js
@@ -0,0 +1,16 @@
+const Comments = () => {
+    // Insturction:
+    // Use endpoint: https://jsonplaceholder.typicode.com/comments to get a list of comments. 
+    // display the comment body on the screen
+    // add event listeners to each comment body that when clicked will simply console.log the comment id.
+
+    // you will need to use the useEffect hook. Do this in async / await syntax.
+
+
+    return(
+        // Replace this with your code.
+        <h1>Comments component</h1>
+    )
+}
+
+export default Comments
